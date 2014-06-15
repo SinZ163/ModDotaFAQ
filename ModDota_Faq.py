184fa1ba900ca4015d6c97b4ec6473f2ae3ef51e
Trent Monahan <sinz@d3s.co>
2014-06-15 14:10:25 +1000

    Added HTML Renderer with proper escaping and fixed = being split more than once

--- ModDota_Faq.py
+++ ModDota_Faq.py
@@ -9,17 +9,45 @@ class ModDota_Faq:
 
     def ReadDB(self):
         with open("commands/ModDota/faqdb.json", "r") as f:
-            jsonres = simplejson.loads(f.read().encode("utf-8"), strict = False )
+            jsonres = simplejson.load(f, encoding = "utf-8", strict = False)
+            #jsonres = simplejson.loads(f.read().encode("utf-8"), strict = False )
             self.db = jsonres
         
     def SaveDB(self):
         with open("commands/ModDota/faqdb.json", "w") as f:
-            f.write(simplejson.dumps(self.db, sort_keys=True, indent=4 * ' ').encode("utf-8"))
+            simplejson.dump(self.db, fp = f, sort_keys=True, indent=4 * ' ', encoding = "utf-8")
+            #f.write(simplejson.dumps(self.db, sort_keys=True, indent=4 * ' ').encode("utf-8"))
             
     def readMsg(self, msg):
         for name, lines in self.db.iteritems():
             if msg.lower() == name.lower():
                 return lines
+                
+class ModDota_Faq_HTMLCompiler:
+    def __init__(self):
+        self.init = False
+        try:
+            self.templates = {}
+            with open("commands/ModDota/faq_template_index.txt", "r") as f:
+                self.templates["index"] = f.read()
+            with open("commands/ModDota/faq_template_entry.txt", "r") as f:
+                self.templates["entry"] = f.read()
+            self.init = True
+        except:
+            #fuck this, they aren't there meaning who ever setup renol failed horribly
+            pass
+    
+    def RenderHTML(self, db):
+        if self.init:
+            try:
+                entryText = ""
+                for name, info in sorted(db.iteritems()):
+                    entryText = entryText + self.templates["entry"].format(entry=(name.encode("utf-8"), ("<br/>".join(info)).encode("utf-8")))
+                with open("commands/ModDota/faqSite/index.html", "w") as f:
+                    f.write(self.templates["index"].format(entry=(entryText)))
+            except:
+                print("OHNO it didn't work, ah well")
+                print(traceback.format_exc())
             
 ID="faq" #this is our command identifier, so with conventional commands, this is the command name
 permission=0 #Min permission required to run the command (needs to be 0 as our lowest command is 0)
@@ -45,6 +73,8 @@ banList = [
 mainPrefix = "??"
 
 modfaq = ModDota_Faq()
+modhtml = ModDota_Faq_HTMLCompiler()
+
 #the command entry point from '=faq" or something
 def execute(self, name, params, channel, userdata, rank):
     try:
@@ -61,7 +91,7 @@ def execute(self, name, params, channel, userdata, rank):
 def __initialize__(self, Startup):
     if self.events["chat"].doesExist("ModDota_Faq"):
         self.events["chat"].removeEvent("ModDota_Faq")
-    self.events["chat"].addEvent("ModDota_Faq", onPrivmsg, False)
+    self.events["chat"].addEvent("ModDota_Faq", onPrivmsg)
 
 def onPrivmsg(self, channels, userdata, message, currChannel):
     #are the first two characters equal to mainPrefix?
@@ -106,7 +136,7 @@ def command_privmsg(self, name, params, channel, userdata, rank):
     result = modfaq.readMsg(args)
     if result:
         for line in result:
-            self.sendMessage(channel, bold+args+":"+bold+" "+line.encode("utf-8"))
+            self.sendMessage(channel, bold+args+":"+bold+" "+line)
     
 def command_notice(self, name, params, channel, userdata, rank):
     #This is where the notice command is.
@@ -123,7 +153,7 @@ def command_target(self, name, params, channel, userdata, rank):
     result = modfaq.readMsg(args)
     if result:
         for line in result:
-            self.sendMessage(channel, bold+target+":"+bold+" ("+args+") "+line.encode("utf-8"))
+            self.sendMessage(channel, bold+target+":"+bold+" ("+args+") "+line)
             
 def command_add(self, name, params, channel, userdata, rank):
     args = " ".join(params[1:])
@@ -132,15 +162,19 @@ def command_add(self, name, params, channel, userdata, rank):
         modfaq.db[info[0]] = info[1].split("||")
         modfaq.SaveDB()
         self.sendMessage(channel, "Added/Modified "+info[0]+", and set it to "+info[1])
+        modhtml.RenderHTML(modfaq.db)
     else:
         self.sendNotice(name, "Bad usage. use it like ??+ name=text")
     
 def command_del(self, name, params, channel, userdata, rank):
     args = " ".join(params[1:])
-    self.sendMessage(channel, "Deleting "+args+", used to say "+"||".join(modfaq.db[args]).encode("utf-8"))
+    self.sendMessage(channel, "Deleting "+args+", used to say "+"||".join(modfaq.db[args]))
     del modfaq.db[args]
     modfaq.SaveDB()
-    
+    modhtml.RenderHTML(modfaq.db)
+
+def debug_compile(self, name, params, channel, userdata, rank):
+    modhtml.RenderHTML(modfaq.db)
 #This is the database for all the commands
 commands = {
     ">" : {
@@ -214,5 +248,10 @@ commands = {
                 "required" : True
             }
         ]
+    },
+    "compile" : {
+        "function" : debug_compile,
+        "rank" : 3,
+        "help" : "Manually compiles the html webpage"
     }
 }
