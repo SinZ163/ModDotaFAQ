95b401d8cde043edcf19c58921528e888d49f75f
ark120202 <ark120202@gmail.com>
2019-12-01 19:36:39 +0000

    Use `dota-data` API format

--- ModDota_APIReborn.py
+++ ModDota_APIReborn.py
@@ -1,4 +1,3 @@
-import simplejson
 import string
 import time
 import traceback
@@ -7,16 +6,139 @@ import requests
 ID="api" #this is our command identifier, so with conventional commands, this is the command name
 permission=0 #Min permission required to run the command (needs to be 0 as our lowest command is 0)
 
-import collections
+colBold = chr(2)
+colBlue = chr(3)+"02"
+colGreen = chr(3)+"03"
+colRed = chr(3)+"04"
+colBrown = chr(3)+"07"
+colEnd = chr(3)
 
-def update(d, u):
-    for k, v in u.iteritems():
-        if isinstance(v, collections.Mapping):
-            r = update(d.get(k, {}), v)
-            d[k] = r
-        else:
-            d[k] = u[k]
-    return d
+def formatArgumentName(name):
+    return colBlue + name + colEnd
+
+def formatFunctionArgument(arg):
+    return formatArgumentName(arg["name"]) + ": " + formatTypes(arg["types"])
+
+def formatFunctionArguments(args):
+    return ", ".join(map(formatFunctionArgument, args))
+
+def formatType(type):
+    if isinstance(type, basestring):
+        return colBrown + type + colEnd
+    elif "array" in type:
+        return "[" + formatTypes(type["array"]) + "]"
+    else:
+        return "{bold}({bold}{args}{bold}){bold} => {returns}".format(
+            bold = colBold,
+            args = formatFunctionArguments(type["args"]),
+            returns = formatTypes(type["returns"])
+        )
+
+def formatTypes(types):
+    return " | ".join(map(formatType, types))
+
+def formatAvailability(availability):
+    return "[{colBlue}{availability}{colEnd}] ".format(
+        colBlue = colBlue,
+        availability = availability.capitalize(),
+        colEnd = colEnd,
+    )
+
+def formatDescription(lines):
+    description = ""
+
+    for line in lines:
+        description += "\n    " if len(lines) > 1 else " "
+        description += "{colGreen}-- {line}{colEnd}".format(
+            colGreen = colGreen,
+            line = line,
+            colEnd = colEnd,
+        )
+
+    return description
+
+def formatCallSignature(function):
+    return "{bold}({bold}{args}{bold}){bold}: {returns}".format(
+        bold = colBold,
+        args = formatFunctionArguments(function["args"]),
+        returns = formatTypes(function["returns"]),
+    )
+
+def formatFunction(function, scopeName):
+    descriptionLines = []
+
+    if "description" in function:
+        descriptionLines += function["description"].splitlines()
+
+    if "deprecated" in function:
+        deprecation = colRed + "Deprecated" + ": " + colGreen + function["deprecated"]
+        descriptionLines += deprecation.splitlines()
+
+    for arg in function["args"]:
+        if not "description" in arg:
+            continue
+
+        argDescription = "{name}{colGreen} - {description}".format(
+            name = formatArgumentName(arg["name"]),
+            colGreen = colGreen,
+            description = arg["description"],
+        )
+
+        descriptionLines += argDescription.splitlines()
+
+    return "{availability}{scopePrefix}{name}{signature}{description}".format(
+        availability = formatAvailability(function["available"]),
+        scopePrefix = scopeName + ":" if scopeName else "",
+        name = function["name"],
+        signature = formatCallSignature(function),
+        description = formatDescription(descriptionLines),
+    )
+
+def formatClass(classDefinition):
+    result = formatAvailability("both" if "clientName" in classDefinition else "server")
+
+    if "instance" in classDefinition:
+        result += classDefinition["instance"] + ": "
+
+    result += classDefinition["name"]
+
+    if "call" in classDefinition:
+        result += formatCallSignature(classDefinition["call"])
+
+    if "extend" in classDefinition:
+        result += " extends " + classDefinition["extend"]
+
+    if "description" in classDefinition:
+        result += formatDescription(classDefinition["description"].splitlines())
+
+    return result
+
+def formatField(field):
+    result = formatArgumentName(field["name"])
+
+    types = list(field["types"])
+    if "nil" in types:
+        types.remove("nil")
+        result += "?"
+
+    result += ": " + formatTypes(types)
+
+    if "description" in field:
+        result += " {colGreen}-- {line}{colEnd}".format(
+            colGreen = colGreen,
+            line = field["description"].replace("\n", " "),
+            colEnd = colEnd,
+        )
+
+    return  result
+
+def formatInterface(interface):
+    fields = ["    " + formatField(field) for field in interface["members"]]
+
+    return "{name}:\n{fields}".format(
+        name = interface["name"],
+        fields = "\n".join(fields),
+    )
 
 class ModDotaAPI:
     def __init__(self):
@@ -25,19 +147,10 @@ class ModDotaAPI:
             'User-agent': 'ModDota_API/1.X (+http://github.com/SinZ163/ModDotaFAQ)'
         }
         self.ReadDump()
-    def fetch_page(self, url, timeout=10, decode_json=True):
-        request = self.requests_session.get(url, timeout=timeout)
-        if decode_json:
-            return request.json()
-        else:
-            return request.text
+    def fetch_page(self, url, timeout=10):
+        return self.requests_session.get(url, timeout=timeout).json()
     def ReadDump(self):
-        #serverInfo = self.fetch_page("https://raw.githubusercontent.com/ModDota/API/master/_data/lua_server.json")
-        serverInfo = self.fetch_page("https://raw.githubusercontent.com/SinZ163/TestTracking/master/lua_server.json")
-        communityInfo = self.fetch_page("https://raw.githubusercontent.com/ModDota/API/master/_data/override_lua_server.json")
-        self.lua_server = serverInfo.copy()
-        self.lua_server = update(self.lua_server, communityInfo)
-        #TODO: add community db here and inject into lua_server
+        self.api = self.fetch_page("https://unpkg.com/dota-data/files/vscripts/api.json")
 
 MDAPI_logger = logging.getLogger("MDAPI_Reborn")
 modDotaAPI = ModDotaAPI()
@@ -48,81 +161,48 @@ def __initialize__(self, Startup):
 #the command entry point from '=api" or something
 def execute(self, name, params, channel, userdata, rank):
     msg = " ".join(params)
-    functions = []
     output = channel
-    #TODO: add logic to figure out which dump we want
-    for Class, ClassInfo in modDotaAPI.lua_server.iteritems():
-        for FunctionName, FunctionInfo in ClassInfo["functions"].iteritems():
-            #print(FunctionName)
-            if msg.lower() in FunctionName.lower():
-                MDAPI_logger.info("Found a method, "+FunctionName)
-                functions.append((Class, FunctionName))
-    if len(functions) == 0:
+
+    topLevelElements = []
+    def checkTopLevelElement(member):
+        if msg.lower() in member["name"].lower():
+            MDAPI_logger.info("Found a top-level element, "+member["name"])
+            topLevelElements.append(member)
+
+    functions = []
+    def checkFunction(function, ScopeName=None):
+        if msg.lower() in function["name"].lower():
+            MDAPI_logger.info("Found a method, "+function["name"])
+            functions.append((function, ScopeName))
+
+    for member in modDotaAPI.api:
+        if member["kind"] == "function":
+            checkFunction(member)
+            continue
+
+        checkTopLevelElement(member)
+        if member["kind"] == "class":
+            for classMember in member["members"]:
+                if classMember["kind"] == "function":
+                    checkFunction(classMember, member["name"])
+
+    matchedCount = len(topLevelElements + functions)
+    if matchedCount == 0:
         self.sendMessage(channel, "No results found.")
-    if len(functions) > 5:
+    if matchedCount > 5:
         #pm it
-        if name == "DB" or len(functions) > 20:
-            self.sendMessage(channel, "Too many functions matched ("+str(len(functions))+"). Please refine your search.")
+        if name == "DB" or matchedCount > 20:
+            self.sendMessage(channel, "Too many elements matched ("+str(matchedCount)+"). Please refine your search.")
             return
         else:
+            self.sendMessage(channel, "Too many elements matched ("+str(matchedCount)+"). Replying privately.")
             output = name
-            self.sendMessage(channel, "Too many functions matched ("+str(len(functions))+"). replying privately.")
-    colBold = chr(2)
-    colItalics = chr(29)
-    colGreen = chr(3)+"03"
-    colBlue = chr(3)+"02"
-    colBrown = chr(3)+"07"
-    colEnd = chr(3)
 
-    for function in functions:
-        className = function[0]
-        functionName = function[1]
-        functionInfo = modDotaAPI.lua_server[className]["functions"][functionName]
-        argInfo = ""
-        description = ""
-        if "args" in functionInfo:
-            if len(functionInfo["args"]) > 0:
-                #We have argument info
-                for index, arg in enumerate(functionInfo["args"]):
-                    if index > 0:
-                        argInfo = argInfo + ", "
-                    if "arg_names" in functionInfo:
-                        if len(functionInfo["arg_names"]) > 0:
-                            #we have argument info with named variables
-                            argInfo = argInfo + u"{nullable}{colBrown}{argType}{colBrown}{nullable} {colBlue}{argName}{colEnd}".format(
-                                colBrown = colBrown,
-                                colBlue = colBlue,
-                                colEnd = colEnd,
-                                argType = arg,
-                                argName = functionInfo["arg_names"][index],
-                                nullable = colItalics if "?" in arg else ""
-                            )
-                            continue
-                    argInfo = argInfo + u"{nullable}{colBrown}{argType}{colEnd}{nullable}".format(
-                        colBrown = colBrown,
-                        colEnd = colEnd,
-                        argType = arg,
-                        nullable = colItalics if "?" in arg else ""
-                    )
-        if argInfo != "":
-            argInfo = " " + argInfo + " "
-        if "description" in functionInfo:
-            description = "{colGreen} -- {description}{colEnd}".format(
-                description = functionInfo["description"],
-                colGreen = colGreen,
-                colEnd = colEnd
-            )
+    for element in topLevelElements:
+        if element["kind"] == "class":
+            self.sendMessage(output, formatClass(element))
+        elif element["kind"] == "interface":
+            self.sendMessage(output, formatInterface(element))
 
-        #self.sendMessage(output, "["+method[0]+"] "+modDotaAPI.db[method[0]]["methods"][method[1]]["return"] + " " + method[1] + colBold+"(" + colBold + msg + colBold+")" + colBold + comment)
-        self.sendMessage(output, "[{colBlue}{className}{colEnd}] {colBrown}{returnType}{colEnd} {name}{bold}({bold}{argInfo}{bold}){bold} {description}".format(
-            bold = colBold,
-            italic = colItalics,
-            colBlue = colBlue,
-            colBrown = colBrown,
-            colEnd = colEnd,
-            className = className,
-            name = functionName,
-            returnType = functionInfo["return"],
-            argInfo = argInfo,
-            description = description
-        ))
+    for (function, scopeName) in functions:
+        self.sendMessage(output, formatFunction(function, scopeName))
